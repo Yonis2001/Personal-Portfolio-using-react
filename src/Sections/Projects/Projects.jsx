9f1a77247686b809ee02522a1721a652c745ba79
Yonis2001 <yonisabdullahi91@gmail.com>
2024-10-02 19:20:14 +0100

    Update Projects.jsx

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -7,26 +7,26 @@ import ProjectCard from '../../common/ProjectCard';
 
 
 function Projects() {
-  return (
-        <section id="projects" className={styles.container}>
-        <h1 className="sectionTitle">Projects</h1>
-  <div className={styles.projectsContainer}>
-      <ProjectCard src={viberr} link={"https://yonis2001.github.io/Bookinder/"}
-      h3="Bookinder"
-      p="Book search app"/>
+      return (
+            <section id="projects" className={styles.container}>
+                  <h1 className="sectionTitle">Projects</h1>
+                  <div className={styles.projectsContainer}>
+                        <ProjectCard src={viberr} link={"https://yonis2001.github.io/Bookinder/"}
+                              h3="Bookinder"
+                              p="Book search app" />
 
-<ProjectCard src={frshBurger} link={"https://www.pythonanywhere.com/user/yonisabdullahi/shares/54e50221c00a4d44b716a5aefbf3be20/"}
-      h3="Rock,paper,Scissors"
-      p="Play a fun game of rock, paper and Scissors "/>
+                        <ProjectCard src={frshBurger} link={"https://github.com/Yonis2001/Rock-paper-scissors"}
+                              h3="Rock,paper,Scissors"
+                              p="Play a fun game of rock, paper and Scissors " />
 
-<ProjectCard src={hipsster} link={"https://yonis2001.github.io/Ferris-wheel/"}
-      h3="Ferris wheel"
-      p="Ferris wheel using CSS Animations"/>
+                        <ProjectCard src={hipsster} link={"https://yonis2001.github.io/Ferris-wheel/"}
+                              h3="Ferris wheel"
+                              p="Ferris wheel using CSS Animations" />
 
-    </div>
-    </section>
-    
-  );
+                  </div>
+            </section>
+
+      );
 }
 
 export default Projects;
